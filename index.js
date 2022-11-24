68748c52d60ec5542994fbe17e21e08b95cc5846
TUYIZERE Samuel <samueltuyizere0@gmail.com>
2022-11-24 22:17:56 +0200

    redirect traffic from mobile.expensa.io as well
    Signed-off-by: TUYIZERE Samuel <samueltuyizere0@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const http = require("http")
 
 const server = http.createServer(function (req, res){
+  if(req.headers.host.includes('mobile.expensa.io')){
+    res.writeHead(302, {
+      location: "https://bit.ly/m/expensa",
+    })
+    res.end()
+  }
   res.writeHead(302, {
     location: "https://expensa.io",
   })
@@ -8,5 +14,5 @@ const server = http.createServer(function (req, res){
 })
 
 server.listen(process.env.PORT, function (){
-  console.log("Server started")
+  console.log("Server started", {port: process.env.PORT})
 })
